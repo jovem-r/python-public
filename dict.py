e2c21ca32425e99246a428d1fa3d177337382bac
jovem-r <145892185+jovem-r@users.noreply.github.com>
2023-09-30 20:51:19 +0800

    Update dict.py

--- dict.py
+++ dict.py
@@ -12,8 +12,8 @@ accounts = {
 
 # 课程链接
 url = {
-        'a': 'https://mooc2-ans.chaoxing.com/mooc2-ans/mycourse/stu?courseid=236648126&clazzid=81564957&cpi=266309580&enc=70d4c87a09da8f2a0c3d9e1cdcc58c28&t=1696077186670&pageHeader=1&v=2',
-        'b': 'https://mooc2-ans.chaoxing.com/mooc2-ans/mycourse/stu?courseid=220017541&clazzid=82050074&cpi=266309580&enc=29ea718347794fe9cb0d6d2ab53ecf45&t=1696077459945&pageHeader=0&v=2',
+        'a': '',
+        'b': '',
         'c': '',
         'd': '',
         'e': '',
