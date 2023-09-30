6ee567c3a8488273eaf9ff900aa1ac87991f49ca
jovem-r <145892185+jovem-r@users.noreply.github.com>
2023-09-30 20:49:58 +0800

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,23 @@
+# 账号 密码 储存字典
+accounts = {
+        'account': '',  # 账户
+        'password': '',  # 密码
+        'account1': '',  # 账户
+        'password1': '',  # 密码
+        'account2': '',  # 账户
+        'password2': '',  # 密码
+        'account3': '',  # 账户
+        'password3': '',  # 密码
+    }
+
+# 课程链接
+url = {
+        'a': 'https://mooc2-ans.chaoxing.com/mooc2-ans/mycourse/stu?courseid=236648126&clazzid=81564957&cpi=266309580&enc=70d4c87a09da8f2a0c3d9e1cdcc58c28&t=1696077186670&pageHeader=1&v=2',
+        'b': 'https://mooc2-ans.chaoxing.com/mooc2-ans/mycourse/stu?courseid=220017541&clazzid=82050074&cpi=266309580&enc=29ea718347794fe9cb0d6d2ab53ecf45&t=1696077459945&pageHeader=0&v=2',
+        'c': '',
+        'd': '',
+        'e': '',
+        'f': '',
+        'g': '',
+        'h': '',
+    }
